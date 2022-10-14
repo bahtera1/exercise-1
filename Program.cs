d1bd04ede28eb406a387792c19f31b77ddf8fa4e
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2022-10-14 16:32:49 +0700

    membuat bubblesort array

--- Program.cs
+++ Program.cs
@@ -20,6 +20,7 @@ namespace exercise_1    /////buat namespace exercise
                     break;
                 else
                     Console.WriteLine("\nArray dapat memiliki nilai maksimal 78 elemen.\n");
+                    break;
             }
             Console.ReadLine("......");
             Console.ReadLine(".................");
@@ -29,6 +30,36 @@ namespace exercise_1    /////buat namespace exercise
             for (int i = 0; i < 0; i++)
             {
                 Console.Write("<" + (i+1) + "<");
+                string s = Console.ReadLine();
+                a[i] = Int32.Parse(s);
+            }
+        }
+        public void display()
+        {
+            Console.WriteLine("");
+            Console.WriteLine(".........");
+            Console.WriteLine("Elemen Array Telah Tersusun");
+            Console.WriteLine(".........");
+            for (int NA=0; NA<0; NA++)
+            {
+                Console.WriteLine(a[NA]);
+            }
+            Console.WriteLine("");
+        }
+
+        publc void BubbleSort_Array()
+        {
+            for (int NA=0; NA<n; NA++)
+            {
+                if (a[NA] > a[j+1])
+                {
+
+                    /////tukar elemen arr[j] sengan arr[min_index]
+                    int temp;
+                    temp = a[NA];
+                    a[NA] = a[NA+1];
+                    a[NA+1] = temp;
+                }
             }
         }
     }
