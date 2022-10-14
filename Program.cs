4dfa7e6bf12db6e44ad8ab1f61f391640f6fd633
Naufal Assani <naufal.assani.ft21@mail.umy.ac.id>
2022-10-14 16:00:11 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace exercise_1
+{
+    class exercise
+    {
+        private int[] a = new int[78];
+
+        private int n;
+
+        public void read()
+        {
+            while(true)
+            {
+                Console.WriteLine("masukan banyak pada elemen array");
+                string s = Console.ReadLine();
+                if(s ==)
+
+            }
+        }
+    }
+}
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+static void Main(string[] args)
+{
+    
+}
